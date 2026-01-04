4ebdefc998a5db19fffacc7b1c70f0e766913213
anshmendiratta <anshmendiratta2023@gmail.com>
2026-01-04 04:12:41 -0500

    .

--- reload_shader.fish
+++ reload_shader.fish
@@ -4,9 +4,11 @@ set OS_IS_LINUX_MINT bash -c "cat /etc/os-release | head -n 1 | grep -q -e 'Linu
 if $OS_IS_LINUX_MINT -eq 0
     # Desktop.
     rm shader.zip
-    set SHADERPACKS_DIR "~/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Shader/minecraft/shaderpacks"
+    zip -r shader.zip shaders/
+
+    set SHADERPACKS_DIR "/home/mintdesktop/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Shader/minecraft/shaderpacks"
     rm $SHADERPACKS_DIR/shader.zip
-    ln -t $SHADERPACKS_DIR shader.zip
+    ln shader.zip $SHADERPACKS_DIR/
 else
     # Asahi.
 end
