6ddb13091bb214373ae7ae00c81bdf91960dc3ee
= <anshmendiratta2023@gmail.com>
2026-01-05 10:12:18 -0500

    .

--- reload_shader.fish
+++ reload_shader.fish
@@ -1,14 +1,15 @@
 #!/home/linuxbrew/.linuxbrew/bin/fish
 
 set OS_IS_LINUX_MINT bash -c "cat /etc/os-release | head -n 1 | grep -q -e 'Linux Mint'"
-if $OS_IS_LINUX_MINT -eq 0
-    # Desktop.
-    rm shader.zip
-    zip -r shader.zip shaders/
+rm shader.zip
+zip -r shader.zip shaders/
 
+if $OS_IS_LINUX_MINT -eq 0
     set SHADERPACKS_DIR "/home/mintdesktop/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Shader/minecraft/shaderpacks"
-    rm $SHADERPACKS_DIR/shader.zip
-    ln shader.zip $SHADERPACKS_DIR/
 else
-    # Asahi.
+    set SHADERPACKS_DIR "/home/asahi/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/Shader/minecraft/shaderpacks"
 end
+rm $SHADERPACKS_DIR/shader.zip
+ln shader.zip $SHADERPACKS_DIR/
+
+echo "Reset shader."
